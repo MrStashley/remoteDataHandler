c54f0bf38ee9e0110dca3a3b22785b5c77635b3b
MrStashley <colel1410@gmail.com>
2019-05-19 13:12:39 -0400

    wtf why doenst it find my dependency

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express'),
       port    = process.env.PORT || 5000;
       path    = require('path');
       bodyParser = require('body-parser');
-      nodemailer = require('nodemailer');
+      nodemailer = require('nodemailer'); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
