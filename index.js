72af970323dbe4fd276a58405a83098b7f0b2678
MrStashley <colel1410@gmail.com>
2019-05-19 11:50:02 -0400

    ahh emailS

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express'),
       http    = require('http').Server(app);
       port    = process.env.PORT || 5000;
       path    = require('path');
+      bodyParser = require('body-parser');
+      nodemailer = require('nodemailer');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'mustache');
 app.engine('mustache', require('hogan-middleware').__express);
@@ -17,7 +21,7 @@ app.get("/test", (req, res, next) =>{
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  
+
   var dataArray = [];
   dataArray[dataArray.length] = 41;
   dataArray[dataArray.length] = 3;
@@ -29,6 +33,16 @@ app.get("/test", (req, res, next) =>{
   res.json(data);
 });
 
+app.post("/email", (req,res,next) =>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  var text = req.body.text;
+
+  console.log(text);
+})
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
